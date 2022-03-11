3644241d421a3175d9036995da6be4c30f6216c7
Seonghoon Oh <su5785@naver.com>
2022-03-11 19:17:15 +0900

    fix: Add credentials option for https network

--- app.js
+++ app.js
@@ -24,7 +24,11 @@ app.use((req, res, next) => {
 });
 
 app.use(logger("dev"));
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
