6535d17ed085c2de119022d94471653c15e94ece
Seonghoon Oh <su5785@naver.com>
2022-03-14 15:06:09 +0900

    fix: firebase login troubleshooting due to COOP, COEP

--- api/routes/firebaseAuth.js
+++ api/routes/firebaseAuth.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.json({ result: "homepage message..." });
+  res.render("login");
 });
 
 module.exports = router;
